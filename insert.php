aa362f86c192ebbd467ae78e06a1e34ab3e4ee97
kshyshieck <kg@b-intohimo.com>
2017-12-13 13:22:09 +0100

    Removed deleted files

--- insert.php
+++
@@ -1,7 +0,0 @@
-<?php
-$atrr = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$db = new PDO('mysql:host={host};dbname={dbname};port=3306', '{dbuser}', '{password}', $atrr);
-$typeName = $db->quote($_GET["typeName"]);
-$db->query('SET NAMES utf8');
-$data = $db->query('INSERT INTO feeding SET feeding_time=NOW(), type_id=(SELECT type_id FROM types WHERE type_name='.$typeName.')');
-header('Content-type: application/json');
--- select.php
+++
@@ -1,7 +0,0 @@
-<?php
-$atrr = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-$db = new PDO('mysql:host={host};dbname={dbname};port=3306', '{dbuser}', '{password}', $atrr);
-$db->query('SET NAMES utf8');
-$data = $db->query('SELECT feeding_id, feeding_time, type_name, comments FROM feeding f inner join types t on f.type_id = t.type_id ORDER BY feeding_id DESC')->fetchAll(PDO::FETCH_ASSOC);
-header('Content-type: application/json');
-echo json_encode($data);
